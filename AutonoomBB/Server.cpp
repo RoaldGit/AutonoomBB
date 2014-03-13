d6dcb05351990955796f0d8f7e4cf8d454974127
Roald <305953@student.saxion.nl>
2014-03-13 14:29:51 +0100

    Server class aangemaakt voor webserver

---
+++ AutonoomBB/Server.cpp
@@ -0,0 +1,18 @@
+/*
+ * Server.cpp
+ *
+ *  Created on: 13 mrt. 2014
+ *      Author: Asus
+ */
+
+#include "Server.h"
+
+Server::Server() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Server::~Server() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ AutonoomBB/Server.h
@@ -0,0 +1,17 @@
+/*
+ * Server.h
+ *
+ *  Created on: 13 mrt. 2014
+ *      Author: Asus
+ */
+
+#ifndef SERVER_H_
+#define SERVER_H_
+
+class Server {
+public:
+	Server();
+	virtual ~Server();
+};
+
+#endif /* SERVER_H_ */
