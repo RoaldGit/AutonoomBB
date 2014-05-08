3946a459ba247ce7d78a36032ba81a11a682579e
Roald <305953@student.saxion.nl>
2014-05-08 19:01:44 +0200

    Added class files for the serial control.

---
+++ AutonoomBB/communication/SerialControl.cpp
@@ -0,0 +1,18 @@
+/*
+ * SerialControl.cpp
+ *
+ *  Created on: 8 mei 2014
+ *      Author: Asus
+ */
+
+#include "SerialControl.h"
+
+SerialControl::SerialControl() {
+	// TODO Auto-generated constructor stub
+
+}
+
+SerialControl::~SerialControl() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ AutonoomBB/communication/SerialControl.h
@@ -0,0 +1,17 @@
+/*
+ * SerialControl.h
+ *
+ *  Created on: 8 mei 2014
+ *      Author: Asus
+ */
+
+#ifndef SERIALCONTROL_H_
+#define SERIALCONTROL_H_
+
+class SerialControl {
+public:
+	SerialControl();
+	virtual ~SerialControl();
+};
+
+#endif /* SERIALCONTROL_H_ */
