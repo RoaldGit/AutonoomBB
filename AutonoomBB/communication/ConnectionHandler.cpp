41348c5d23d4810759942348ddbee77d87ff3629
Roald <305953@student.saxion.nl>
2014-05-23 11:24:13 +0200

    Added thread class that will handle the trafic on the socket created by accept().

---
+++ AutonoomBB/communication/ConnectionHandler.cpp
@@ -0,0 +1,20 @@
+/*
+ * ConnectionHandler.cpp
+ *
+ *  Created on: 23 mei 2014
+ *      Author: Asus
+ */
+
+#include "ConnectionHandler.h"
+
+namespace std {
+
+ConnectionHandler::~ConnectionHandler() {
+	// TODO Auto-generated destructor stub
+}
+ConnectionHandler::ConnectionHandler() {
+	// TODO Auto-generated constructor stub
+
+}
+
+} /* namespace std */
---
+++ AutonoomBB/communication/ConnectionHandler.h
@@ -0,0 +1,22 @@
+/*
+ * ConnectionHandler.h
+ *
+ *  Created on: 23 mei 2014
+ *      Author: Asus
+ */
+
+#ifndef CONNECTIONHANDLER_H_
+#define CONNECTIONHANDLER_H_
+
+namespace std {
+
+class ConnectionHandler {
+public:
+	virtual ~ConnectionHandler();
+
+private:
+	ConnectionHandler();
+};
+
+} /* namespace std */
+#endif /* CONNECTIONHANDLER_H_ */
