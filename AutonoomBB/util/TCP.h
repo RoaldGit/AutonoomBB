8c7a8e4d5938a63700782efd8eb956945011f370
Roald <305953@student.saxion.nl>
2014-06-01 13:31:28 +0200

    Added class containing struct for TPC packets.

---
+++ AutonoomBB/util/TCP.h
@@ -0,0 +1,26 @@
+/*
+ * TCP.h
+ *
+ *  Created on: 31 mei 2014
+ *      Author: Roald
+ *
+ */
+
+#ifndef TCP_H_
+#define TCP_H_
+
+struct tpc_header
+{
+	int16_t source_port;
+	int16_t dest_port;
+	int32_t seq_num;
+	int32_t ack_num;
+	int data_offset;
+	char reserved[3];
+	char flags[9];
+	int16_t window;
+	int16_t checksum;
+	int16_t urgent_pointer;
+};
+
+#endif /* TCP_H_ */
